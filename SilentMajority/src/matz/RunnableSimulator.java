e82f1320c410752606bcb1d1885145981271aa56
YuMatsuzawa <celestial.rhapsody.0824@gmail.com>
2014-01-08 18:04:05 +0900

    Framework set.

--- SilentMajority/src/matz/RunnableSimulator.java
+++ SilentMajority/src/matz/RunnableSimulator.java
@@ -1,13 +1,41 @@
 package matz;
 
-public class RunnableSimulator {
+public class RunnableSimulator implements Runnable {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+	private double SilentAgentsRatio;
+	private double ModelReferenceRatio;
+	
+	public void setSilentAgentsRatio(double silentAgentsRatio) {
+		SilentAgentsRatio = silentAgentsRatio;
+	}
+
+	public double getSilentAgentsRatio() {
+		return SilentAgentsRatio;
+	}
+
+	public double getModelReferenceRatio() {
+		return ModelReferenceRatio;
+	}
+
+	public void setModelReferenceRatio(double modelReferenceRatio) {
+		ModelReferenceRatio = modelReferenceRatio;
+	}
+
+	public RunnableSimulator() {
+		setSilentAgentsRatio(Math.random());
+		setModelReferenceRatio(Math.random());
+	}
+	
+	public RunnableSimulator(double silentAgentsRatio, double modelReferenceRatio) {
+		setSilentAgentsRatio(silentAgentsRatio);
+		setModelReferenceRatio(modelReferenceRatio);
+	}
+	
+	@Override
+	public void run() {
 		// TODO 自動生成されたメソッド・スタブ
-
+		System.out.println();
+		
 	}
 
 }
---
+++ SilentMajority/src/matz/SimulationExecutor.java
@@ -0,0 +1,13 @@
+package matz;
+
+public final class SimulationExecutor {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
