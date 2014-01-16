bb722a592cf04886a05028f682adae0732489ef7
Yu <Yu@10.208.68.212>
2014-01-16 20:09:29 +0900

    InfoAgent updated. several methods and fields.

--- SilentMajority/src/matz/agentsim/InfoAgent.java
+++ SilentMajority/src/matz/agentsim/InfoAgent.java
@@ -10,6 +10,8 @@ public class InfoAgent {
 	private ArrayList<String> followedNameList;
 	private ArrayList<Integer> followingIndexList;
 	private ArrayList<Integer> followedIndexList;
+	private boolean isSilent = false;
+	private int Opinion;
 	/**情報エージェントの文字列名を取得．
 	 * @return
 	 */
@@ -69,6 +71,76 @@ public class InfoAgent {
 	 */
 	public void appendFollowedNameList(String name) {
 		this.followedNameList.add(name);
+	}
+	/**情報エージェントが参照しているエージェントの整数インデックスリストを取得。
+	 * @return followingIndexList
+	 */
+	public ArrayList<Integer> getFollowingIndexList() {
+		return followingIndexList;
+	}
+	/**情報エージェントが参照しているエージェントの整数インデックスリストを空のリストに初期化。
+	 * @param followingIndexList セットする followingIndexList
+	 */
+	public void initFollowingIndexList() {
+		this.followingIndexList = new ArrayList<Integer>();
+	}
+	/**情報エージェントが参照しているエージェントの整数インデックスリストに新たなエージェントの整数インデックスを追加。
+	 * @param followingIndexList セットする followingIndexList
+	 */
+	public void initFollowingIndexList(int index) {
+		this.followingIndexList.add(index);
+	}
+	/**情報エージェントが参照されているエージェントの整数インデックスリストを取得。
+	 * @return followedIndexList
+	 */
+	public ArrayList<Integer> getFollowedIndexList() {
+		return followedIndexList;
+	}
+	/**情報エージェントが参照されているエージェントの整数インデックスリストを空のリストに初期化。
+	 * @param followedIndexList セットする followedIndexList
+	 */
+	public void initFollowedIndexList() {
+		this.followedIndexList = new ArrayList<Integer>();
+	}
+	/**情報エージェントが参照されているエージェントの整数インデックスリストに新たなエージェントの整数インデックスを追加。
+	 * @param followedIndexList セットする followedIndexList
+	 */
+	public void appendFollowedIndexList(int index) {
+		this.followedIndexList.add(index);
+	}
+	/**情報エージェントがサイレントであればtrue，ヴォーカルであればfalseを返す<br />
+	 * デフォルトはfalse
+	 * @return isSilent
+	 */
+	public boolean isSilent() {
+		return isSilent;
+	}
+	/**情報エージェントをサイレントにする。
+	 */
+	public void muzzle() {
+		this.isSilent = true;
+	}
+	/**情報エージェントをヴォーカルにする。
+	 */
+	public void unmuzzle() {
+		this.isSilent = false;
+	}
+	/**情報エージェントの現在の意見を取得する。<br />
+	 * エージェントがサイレントである場合は取得できない。null値を返す
+	 * @return opinion
+	 */
+	public int getOpinion() {
+		if (!this.isSilent()) {
+			return Opinion;
+		} else {
+			return (Integer)null;
+		}
+	}
+	/**情報エージェントの意見を指定する。
+	 * @param opinion セットする opinion
+	 */
+	public void setOpinion(int opinion) {
+		Opinion = opinion;
 	}
 	
 	
