ab250896ac5d0fac73ef80bca6e8e5f3f3503552
YuMatsuzawa <celestial.rhapsody.0824@gmail.com>
2015-01-08 20:04:59 +0900

    network importing impl. not tested yet

--- SilentMajority/src/matz/basics/network/StaticCSTMNetwork.java
+++ SilentMajority/src/matz/basics/network/StaticCSTMNetwork.java
@@ -3,6 +3,7 @@ package matz.basics.network;
 import java.io.*;
 import java.util.ArrayList;
 
+
 /**所与のネットワークファイルを読み込んでネットワークインスタンスを構築するクラス．<br>
  * ファイル形式は鈴村研データのCSV形式に従う．即ち，一行の中身が，<br>
  * <code>userID,#followed,#following,follower csv,followee csv</code><br>
@@ -12,17 +13,35 @@ import java.util.ArrayList;
  *
  */
 public class StaticCSTMNetwork extends StaticNetwork {
+	private final static int USERID_INDEX = 0, NUM_FOLLOWED_INDEX = 1, NUM_FOLLOWING_INDEX = 2;
 	
 	private static ArrayList<String[]> customNetworkList = null;
 
-	/* 指定されたネットワークファイルを読み込み，
+	/* インスタンス化の際にファイルから読み込んで作成し，さらにランダムサンプリングで要素数を削ったリストをシーケンシャルに読み，各要素内のCSVから，存在しないリンクへのノードを無視しつつ，ネットワークリストに追加していく．
 	 * (非 Javadoc)
 	 * @see matz.basics.network.StaticNetwork#build()
 	 */
 	@Override
 	public void build() {
-		// TODO 自動生成されたメソッド・スタブ
-
+		for (int index = 0; index < nAgents; index++) {
+			String[] csv = customNetworkList.get(index);
+			for (int cursor = 3; cursor <= 2 + Integer.valueOf(csv[NUM_FOLLOWED_INDEX]); cursor++) {	//FollowedListをIntegerIndexに変換してアペンド
+				for (int innerIndex = 0; innerIndex < nAgents; innerIndex++) {
+					if (customNetworkList.get(innerIndex)[USERID_INDEX].equals(csv[cursor])) {		//ノードのListにあるuserIDがサンプル全体Listに含まれているか判定
+						this.appendToFollowedListOf(index, innerIndex);		 //含まれていれば，対象userIDのノードが格納されているindexを，自ノードindexのFollowedListにアペンド．
+						break;	//見つかったならループは閉じる．見つからなかった場合は単に無視して次に行く．
+					}
+				}
+			}
+			for (int cursor = 3 + Integer.valueOf(csv[NUM_FOLLOWING_INDEX]); cursor < csv.length; cursor++) {	//FollowingListをIntegerIndexに変換してアペンド
+				for (int innerIndex = 0; innerIndex < nAgents; innerIndex++) {
+					if (customNetworkList.get(innerIndex)[USERID_INDEX].equals(csv[cursor])) {		//ノードのListにあるuserIDがサンプル全体Listに含まれているか判定
+						this.appendToFollowingListOf(index, innerIndex);		 //含まれていれば，対象userIDのノードが格納されているindexを，自ノードindexのFollowingListにアペンド．
+						break;	//見つかったならループは閉じる．見つからなかった場合は単に無視して次に行く．
+					}
+				}
+			}
+		}
 	}
 
 
@@ -53,10 +72,18 @@ public class StaticCSTMNetwork extends StaticNetwork {
 		br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(customNetworkPath))));
 		String line = "";
 		while((line = br.readLine()) != null) {
-			customNetworkList.add(line.split(","));
+			String[] keyval = line.split("\t");		//元ネットワークファイルはuserid-csv pair
+			String[] csv = keyval[1].split(",");
+			if (csv[NUM_FOLLOWED_INDEX].equals("-1") || csv[NUM_FOLLOWING_INDEX].equals("-1")) continue;	//鍵付きユーザを除外
+			
+			customNetworkList.add(csv);
 		}
 		br.close();
-		//TODO
+		
+		while (customNetworkList.size() > nAgents) {//sampling
+			int index = localRNG.nextInt(customNetworkList.size());
+			customNetworkList.remove(index);
+		}
 
 		//Listに取り込んだ元データはノードをLong型のUserIDで管理しているため，そのままだとInteger型のネットワークリストにマッチしない．
 		//StaticNetworkの派生クラス，及びInfoAgentクラスでは，ノード（エージェント）の識別をint整数のindexで行っており，このindexはネットワークリストのindexと対応しているからである．
